183348eeae9780af1ce1b0082509b5ea6bc4493e
lzh667 <981445273@qq.com>
2026-07-09 20:51:58 +0800

    ShineConnoisseur

---
+++ src/main/java/com/lzh/service/IMovieFavoriteService.java
@@ -0,0 +1,7 @@
+package com.lzh.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lzh.po.MovieFavorite;
+
+public interface IMovieFavoriteService extends IService<MovieFavorite> {
+}
