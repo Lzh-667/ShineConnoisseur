1c109ad4edd27fbcb0efc0629e17c05888fcfe87
lzh667 <981445273@qq.com>
2026-07-09 19:45:19 +0800

    ShineConnoisseur

--- src/main/java/com/lzh/cache/MovieCacheTask.java
+++ src/main/java/com/lzh/cache/MovieCacheTask.java
@@ -2,15 +2,21 @@ package com.lzh.cache;
 
 import com.lzh.service.IMovieService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class MovieCacheTask {
     @Resource
     private IMovieService movieService;
     @Scheduled(cron = "0 */30 * * * ?")
     public void refreshHotMovie(){
-        movieService.updateHotMovieCache();
+        try {
+            movieService.updateHotMovieCache();
+        }catch(Exception e){
+            log.error("刷新热门电影缓存失败",e);
+        }
     }
 }
