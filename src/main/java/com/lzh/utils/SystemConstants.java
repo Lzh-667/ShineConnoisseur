0f6017f51fb8dd4be11fa5c30a5d9aa217ca9029
lzh667 <981445273@qq.com>
2026-07-13 23:28:03 +0800

    ShineConnoisseur

--- src/main/java/com/lzh/utils/SystemConstants.java
+++ src/main/java/com/lzh/utils/SystemConstants.java
@@ -1,7 +1,6 @@
 package com.lzh.utils;
 
 public class SystemConstants {
-    public static final int DEFAULT_PAGE_SIZE = 5;
     public static final int MAX_PAGE_SIZE = 10;
 
     public static final int TARGET_COMMENT = 1;
