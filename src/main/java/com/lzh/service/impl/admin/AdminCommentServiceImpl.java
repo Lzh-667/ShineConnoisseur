7c49685282c7b2e841e81e6d0685ecb2934bb5a5
lzh667 <981445273@qq.com>
2026-07-16 20:22:34 +0800

    ShineConnoisseur

--- src/main/java/com/lzh/service/impl/admin/AdminCommentServiceImpl.java
+++ src/main/java/com/lzh/service/impl/admin/AdminCommentServiceImpl.java
@@ -13,7 +13,6 @@ import com.lzh.vo.AdminCommentVO;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +25,7 @@ public class AdminCommentServiceImpl implements IAdminCommentService {
 
     @Resource
     private IReviewCommentService reviewCommentService;
-    @Autowired
+    @Resource
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
