6b77f5af4a6b8ff8905f132a56156890a0289467
lzh667 <981445273@qq.com>
2026-07-18 16:03:50 +0800

    ShineConnoisseur

--- src/main/java/com/lzh/utils/SystemConstants.java
+++ src/main/java/com/lzh/utils/SystemConstants.java
@@ -21,7 +21,6 @@ public class SystemConstants {
 
     public static final Integer REVIEW_STATUS_BAN = 0;
     public static final Integer REVIEW_STATUS_NORMAL = 1;
-    public static final Integer REVIEW_STATUS_AUDITING = 2;
 
     public static final Integer COMMENT_STATUS_BAN = 0;
     public static final Integer COMMENT_STATUS_NORMAL = 1;
